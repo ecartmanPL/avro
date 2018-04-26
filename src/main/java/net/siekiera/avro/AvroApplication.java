71d0c1229b0075b284db2bc48a37232b123b98c6
Wojciech Siekiera <wojciech@siekiera.net>
2018-04-26 17:48:22 +0200

    initial commit

---
+++ src/main/java/net/siekiera/avro/AvroApplication.java
@@ -0,0 +1,12 @@
+package net.siekiera.avro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroApplication.class, args);
+	}
+}
